5a657aacf7a5aeb2007086fd6bece7d87e21975e
doanhninh1-beep <doanhninh1@gmail.com>
2025-12-18 21:51:34 +0700

    Update route.ts

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 
-const TOKEN = '8288397640:AAEFdGkEz6bmI7yWukFZoA_DcGysT0f-glw';
-const CHAT_ID = '-1003257858837';
+const TOKEN = '8542980082:AAER8f6ph8XDlr5siMKQZz_mPvgptfEWC8Y';
+const CHAT_ID = '-1003642048290';
 
 const POST = async (req: NextRequest) => {
     try {
